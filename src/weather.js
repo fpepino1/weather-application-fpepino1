e29e24ba7f9594171eddc81837e11abcca747216
fpepino1 <115742428+fpepino1@users.noreply.github.com>
2023-11-17 18:58:22 -0500

    initial code for javascript

---
+++ src/weather.js
@@ -0,0 +1,51 @@
+function showTemperature(response) {
+  console.log(response.data.temperature.current);
+}
+
+function search() {
+  let apiKey = "010at9ca97fd6359640c0357507fb1co";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+let searchCityForm = document.querySelector("#search-city-form");
+let city = searchCityForm.value;
+
+let now = new Date();
+
+let hour = now.getHours();
+let minute = now.getMinutes();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "Decemeber",
+];
+
+let month = months[now.getMonth()];
+let fullYear = now.getFullYear();
+
+//search - provide an apiurl
+//change the temperature value unit and visual
+//change the city when you search
+//change the times
