52fee2a8dcf49556660795f5ed326d2eb2691cd0
fpepino1 <115742428+fpepino1@users.noreply.github.com>
2023-11-17 21:08:28 -0500

    integrated API for the application

--- src/weather.js
+++ src/weather.js
@@ -1,55 +1,64 @@
-function showTemperature(response) {
-  console.log(response.data.temperature.current);
+function refreshWeather(response) {
+  let currentTemperatureElement = document.querySelector(
+    "#current-temperature-value"
+  );
+  let currentTemperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = response.data.city;
+  currentTemperatureElement.innerHTML = Math.round(currentTemperature);
+
+  let currentDay = document.querySelector("#current-day");
+  let current 
+}
+
+function searchCity(city) {
+  let apiKey = "010at9ca97fd6359640c0357507fb1co";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
 }
 
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchCityInput = document.querySelector("#search-city-input");
-  let cityElement = document.querySelector("#current-city");
-  let city = searchCityInput.value;
-  cityElement.innerHTML = `${city}`;
-
-  let apiKey = "010at9ca97fd6359640c0357507fb1co";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showTemperature);
+  searchCity(searchCityInput.value);
 }
 
-let searchCityForm = document.querySelector("#search-city-form");
-searchCityForm.addEventListener("submit", handleSearchSubmit);
+let searchCityFormElement = document.querySelector("#search-city-form");
+searchCityFormElement.addEventListener("submit", handleSearchSubmit);
 
-let now = new Date();
+// let now = new Date();
 
-let hour = now.getHours();
-let minute = now.getMinutes();
+// let hour = now.getHours();
+// let minute = now.getMinutes();
 
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[now.getDay()];
+// let days = [
+//   "Sunday",
+//   "Monday",
+//   "Tuesday",
+//   "Wednesday",
+//   "Thursday",
+//   "Friday",
+//   "Saturday",
+// ];
+// let day = days[now.getDay()];
 
-let months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "Decemeber",
-];
+// let months = [
+//   "January",
+//   "February",
+//   "March",
+//   "April",
+//   "May",
+//   "June",
+//   "July",
+//   "August",
+//   "September",
+//   "October",
+//   "November",
+//   "Decemeber",
+// ];
 
-let month = months[now.getMonth()];
-let fullYear = now.getFullYear();
+// let month = months[now.getMonth()];
+// let fullYear = now.getFullYear();
 
 //search - provide an apiurl
 //change the temperature value unit and visual
